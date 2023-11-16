924a9d3a64f01e901f52ca5212a2e5f335440f04
AlinNast <alin10nastasa@gmail.com>
2023-11-15 16:53:43 -0800

    made the form a modal

--- coman-clean/src/FormModal/FormModal.js
+++ coman-clean/src/FormModal/FormModal.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './FormModal.css';
 
-const FormModal = () => {
+const FormModal = ( {toggleModal} ) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -12,6 +12,12 @@ const FormModal = () => {
         dateToClean:''
 
     });
+
+    const handleToggle = () => {
+        console.log("im here", toggleModal);
+        
+        toggleModal(false);
+    };
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -25,32 +31,41 @@ const FormModal = () => {
     };
 
     return (
-        <div className="contact-form-container">
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
 
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+        <div className="modal-overlay" onClick={handleToggle}>
+            <div className="modal-content" onClick={e => e.stopPropagation()}>
+                <div className="contact-form-container">
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
 
-                <label htmlFor="telephone">Telephone:</label>
-                <input type="text" id="telephone" name="telephone" value={formData.telephone} onChange={handleChange} />
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
 
-                <label htmlFor="address">Address:</label>
-                <input type="text" id="address" name="address" value={formData.address} onChange={handleChange} />
+                        <label htmlFor="telephone">Telephone:</label>
+                        <input type="text" id="telephone" name="telephone" value={formData.telephone} onChange={handleChange} />
 
-                <label htmlFor="postcode">Postcode:</label>
-                <input type="text" id="postcode" name="postcode" value={formData.postcode} onChange={handleChange} />
+                        <label htmlFor="address">Address:</label>
+                        <input type="text" id="address" name="address" value={formData.address} onChange={handleChange} />
 
-                <label htmlFor="dateToClean">Date to Clean:</label>
-                <input type="date" id="dateToClean" name="dateToClean" value={formData.dateToClean} onChange={handleChange} />
+                        <label htmlFor="postcode">Postcode:</label>
+                        <input type="text" id="postcode" name="postcode" value={formData.postcode} onChange={handleChange} />
 
-                <label htmlFor="message">Message:</label>
-                <textarea id="message" name="message" value={formData.message} onChange={handleChange} required />
+                        <label htmlFor="dateToClean">Date to Clean:</label>
+                        <input type="date" id="dateToClean" name="dateToClean" value={formData.dateToClean} onChange={handleChange} />
 
-                <button type="submit">Send Message</button>
-            </form>
+                        <label htmlFor="message">Message:</label>
+                        <textarea id="message" name="message" value={formData.message} onChange={handleChange} required />
+
+                        <button type="submit">Send Message</button>
+                    </form>
+                </div>
+                {/* Close Button */}
+                <button className="close-button" onClick={handleToggle}>Close</button>
+            
+            </div>
         </div>
+
     );
 };
 
--- coman-clean/src/Homepage/Homepage.js
+++ coman-clean/src/Homepage/Homepage.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Carousel CSS
 import './Homepage.css';
+import FormModal from '../FormModal/FormModal'
 
 // Coman facility services
 
 const Homepage = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const toggleModal = () => {
+        console.log("im here");
+        setIsModalOpen(!isModalOpen);
+        };
+
   return (
       <div>
           <section className="hero">
               <h1>Your Space, Sparkling Clean!</h1>
+            <h2>
+                <div>
+                    <br/>
+                    <button onClick={toggleModal}>butonul</button>
+            {isModalOpen && <FormModal toggleModal={setIsModalOpen} />}
+                </div>
+            </h2>
           </section>
           <section className="project-carousel">
               <Carousel autoPlay interval={3000} infiniteLoop useKeyboardArrows>
